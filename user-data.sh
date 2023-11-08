11ab0a96d6f8ef88f14340d9574a48183dc9fbe1
Jonathan Fotso <jonasfost@gmail.com>
2023-11-08 16:59:28 -0500

    updates user-data.sh file

--- user-data.sh
+++ user-data.sh
@@ -19,11 +19,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 
-
 #install the Docker packages. 
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
 
 # Start and Enable Docker 
 sudo service docker start
