3f29cf2eed7cc96a0512316777e950dbfd8bf3e2
Jonathan Fotso <jonasfost@gmail.com>
2023-11-09 11:22:42 -0500

    updated providers.tf file

--- providers.tf
+++ providers.tf
@@ -2,14 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 5.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 #######################################
@@ -29,7 +24,7 @@ terraform {
     organization = "jonasfost"
 
     workspaces {
-      name = "prod-us-east-2-infrastructure"
+      name = "prod-aws-infrastructure"
     }
   }
 }
