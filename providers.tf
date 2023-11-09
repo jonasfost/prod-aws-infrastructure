dfca9f60aca09f03f89c624075081841313ba76e
Jonathan Fotso <jonasfost@gmail.com>
2023-11-09 11:14:38 -0500

    updated providers.tf file

--- providers.tf
+++ providers.tf
@@ -2,16 +2,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
     }
   }
+  required_version = ">= 1.1.0"
 }
 
 #######################################
 ###### Configure the AWS Provider #####
 #######################################
 
-provider "registry.terraform.io/hashicorp/aws" {
+provider "aws" {
   region = "us-east-2"
 }
 
